aa1b4bd440b16a2d785bd014f15fefd448abfa69
sawsanCS <sawsan_sli3i@live.fr>
2021-01-18 17:09:21 -0500

    used classNames in button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,11 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from 'classnames';
 
 export default function Button(props) {
-  let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-
-  if (props.danger) {
-    buttonClass += " button--danger";
-  }
+  const buttonClass = classNames({
+    button: true, 'button--confirm': props.confirm,'button--danger': props.danger })
 
   return (
     <button
