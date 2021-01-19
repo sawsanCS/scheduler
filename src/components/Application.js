b9c8fab6f3aedbe3a005450a7896e95981d9bd13
sawsanCS <sawsan_sli3i@live.fr>
2021-01-19 12:50:16 -0500

    rendering the list to the main page

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 import "components/Application.scss";
-import "components/";
+import DayList from 'components/DayList';
 
-
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 export default function Application(props) {
   return (
     <main className="layout">
@@ -14,14 +30,21 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={"Monday"}
+            setDay={day => console.log(day)}
+          />
+
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
 
-       
+
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
