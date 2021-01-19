7689fc25c1ef6c8dccd58850bc2b83adb634c101
sawsanCS <sawsan_sli3i@live.fr>
2021-01-19 12:26:28 -0500

    implemented DayList.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,11 +1,22 @@
 import React from "react";
+import DayListItem from "./DayListItem";
 
 
 
-export default function DayList() {
+export default function DayList(props) {
+  props.days.map((day) => {
+    return (
 
-  return (
-    <ul></ul>
-   
-  );
+      <ul>
+        <DayListItem
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay} />
+
+      </ul>
+
+    );
+  })
+
 }
