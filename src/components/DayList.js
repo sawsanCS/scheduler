e680d8aa92ac7e61a8fd8b710b7fab5069df2d38
sawsanCS <sawsan_sli3i@live.fr>
2021-01-19 12:37:14 -0500

    displaying all elements DayList.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,19 +4,20 @@ import DayListItem from "./DayListItem";
 
 
 export default function DayList(props) {
-  props.days.map((day) => {
-    return (
-
-      <ul>
-        <DayListItem
-          name={day.name}
-          spots={day.spots}
-          selected={day.name === props.day}
-          setDay={props.setDay} />
-
-      </ul>
-
-    );
-  })
+  const dayListContent = props.days.map(day => (
+    <DayListItem
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  ))
+  return (<ul>
+    {dayListContent}
+  </ul>);
 
 }
+
+
+
+
