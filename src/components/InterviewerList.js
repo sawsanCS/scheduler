7556f120b3d9380fb228bfcc023ec141a2c602f2
sawsanCS <sawsan_sli3i@live.fr>
2021-01-19 23:58:18 -0500

    updated the setInterviewer

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,17 +1,22 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList(props) {
-  const InterviewerListContent = props.interviewers.map(interviewer => (
-    <InterviewerListItem
-    key = {interviewer.id}
-    name = {interviewer.name}
-    avatar = {interviewer.avatar}
-    selected = {interviewer.id === props.value}
-    setInterviewer = {event => props.onChange(interviewer.id)}    />
-  ))
-  return (<ul>
-    {InterviewerListContent}
-  </ul>);
+import "./InterviewerList.scss";
 
+
+export default function InterviewerList(props) {
+  const interviewersContent = props.interviewers.map(interviewer => (
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer ={(event) => props.setInterviewer(interviewer.id)}
+       />
+  ))
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewersContent}</ul>
+    </section>
+  );
 }
