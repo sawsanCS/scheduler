98301b3bdf5086187db731f3c4da53f65ceb6574
sawsanCS <sawsan_sli3i@live.fr>
2021-01-25 13:46:51 -0500

    calling the custom hook useApplicationData

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,144 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "components/Application.scss";
+import useApplicationData from "../hooks/useApplicationData"
 import DayList from 'components/DayList';
-import axios from 'axios';
-import { getAppointmentsForDay, getInterview } from 'helpers/selectors';
 
-import InterviewerList from "components/InterviewerList";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from 'helpers/selectors';
+
 import Appointment from 'components/Appointment';
 
-/*const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];*/
-/* const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
- *//* const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Saoussen Slii",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "2pm",
-  },
-]; */
 
 export default function Application(props) {
  
-console.log('props of App', props)
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-   appointments: {},
-   interviewers: {}
-  })
-  let dailyAppointments = [];
-  dailyAppointments = getAppointmentsForDay(state, state.day)
+    const {
+      state,
+      setDay,
+      bookInterview,
+      cancelInterview
+    } = useApplicationData();
   
-  const setDay = day => setState(prev => ({ ...prev, day }));
-
-  const scheduleList = dailyAppointments.map(appointment => {
-    const interview = getInterview(state, appointment.interview);
-     return (<Appointment 
-    key={appointment.id}
-    id={appointment.id}
-    time={appointment.time}
-    interview={interview}
-    />) }
-  )
-  //using a hook to get days from the API
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments')),
-      Promise.resolve(axios.get('/api/interviewers')),
-
-    ]).then(all => {
-      const days = all[0].data
-      const appointments = all[1].data
-      const interviewers = all[2].data
-      console.log('here are the interviewers', interviewers)
-      console.log('days are', days, 'and appointments are ', appointments)
-      setState(prev => ({
-        ...prev,
-        days,
-        appointments: getAppointmentsForDay({ days, appointments }, state.day),
-        interviewers
-      }))
-    })
-  })
+    const interviewers = getInterviewersForDay(state, state.day);
   
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList 
-            days={state.days}
-            day={state.day}
-            setDay={(day) => setDay(day)}
+    const appointments = getAppointmentsForDay(state, state.day).map(
+      appointment => {
+        return (
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+            interview={getInterview(state, appointment.interview)}
+            interviewers={interviewers}
+            bookInterview={bookInterview}
+            cancelInterview={cancelInterview}
           />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-
-
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {scheduleList}
-      </section>
-    </main>
-  )
-}
+        );
+      }
+    );
+  
+    return (
+      <main className="layout">
+        <section className="sidebar">
+          <img
+            className="sidebar--centered"
+            src="images/logo.png"
+            alt="Interview Scheduler"
+          />
+          <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+            <DayList days={state.days} day={state.day} setDay={setDay} />
+          </nav>
+          <img
+            className="sidebar__lhl sidebar--centered"
+            src="images/lhl.png"
+            alt="Lighthouse Labs"
+          />
+        </section>
+        <section className="schedule">
+          <section className="schedule">
+            {appointments}
+            <Appointment key="last" time="5pm" />
+          </section>
+        </section>
+      </main>
+    );
+  }
+  
