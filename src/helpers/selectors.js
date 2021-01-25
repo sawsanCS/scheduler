830c77e3e5e302346f828494d0c65a40c216a39e
sawsanCS <sawsan_sli3i@live.fr>
2021-01-25 13:50:06 -0500

    adding helper functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,7 +3,10 @@ export function selectUserByName(state, name) {
   const filteredNames = state.users.filter(user => user.name === name)
   return filteredNames
 }
-
+export function getInterview (state, interview){
+  if (!interview) return null;
+  return {interviewer: state.interviewers[interview.interviewer], student: interview.student};
+}
 export function getAppointmentsForDay(state, day) {
   const filteredAppointmentsForDay = []
   state.days.forEach(stateDay => {
@@ -13,5 +16,18 @@ export function getAppointmentsForDay(state, day) {
       })
     }
   })
+
   return filteredAppointmentsForDay
+}
+
+export function getInterviewersForDay(state, day) {
+  const filteredInterviewers = []
+  state.days.forEach(stateDay => {
+    if (stateDay.name === day) {
+      stateDay.interviewers.forEach(interviewerId => {
+        filteredInterviewers.push(state.interviewers[interviewerId])
+      })
+    }
+  })
+  return filteredInterviewers
 }
