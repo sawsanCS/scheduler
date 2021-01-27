84ccfc40fdc39d8bca698c18eee3f06ba0881a61
sawsanCS <sawsan_sli3i@live.fr>
2021-01-26 19:10:46 -0500

    first test refactored

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement} from "@testing-library/react";
 import Application from "components/Application";
-
+import { fireEvent } from '@testing-library/react/dist'
 afterEach(cleanup);
-
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+describe('Application', () => {
+it("defaults to Monday and changes the schedule when a new day is selected",  () => {
   const { getByText } = render(<Application />);
 
   return waitForElement(() => getByText("Monday")).then(() => {
@@ -14,3 +14,4 @@ it("defaults to Monday and changes the schedule when a new day is selected", () 
   });
 });
 
+});
