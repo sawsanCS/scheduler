ff5fbc36ea7a43a57bfcd7ffd7be9889be5694a0
sawsanCS <sawsan_sli3i@live.fr>
2021-01-26 19:19:13 -0500

    replace the test with async call

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -5,13 +5,15 @@ import Application from "components/Application";
 import { fireEvent } from '@testing-library/react/dist'
 afterEach(cleanup);
 describe('Application', () => {
-it("defaults to Monday and changes the schedule when a new day is selected",  () => {
-  const { getByText } = render(<Application />);
-
-  return waitForElement(() => getByText("Monday")).then(() => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+  
     fireEvent.click(getByText("Tuesday"));
+  
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
-});
+  
 
 });
